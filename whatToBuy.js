fad250330666e04eb5e447e6783fd129415f6e1c
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-05-25 14:41:23 +0200

    passing a test and adding an event

--- whatToBuy.js
+++ whatToBuy.js
@@ -1,9 +1,22 @@
+const checkbutton = document.querySelector('.button');
+const cupcakeElement = document.querySelector('.cupcake_price');
+const muffinElement = document.querySelector('.muffin_price');
+const message = document.querySelector('.message')
+
 
 // this function compares the price of a muffin & a cupcake
-function whatToBuy(cupcake_price, muffin_price){
-	
-	// Buy a muffin
+function whatToBuy(){
+	var cupcakeValue = Number(cupcakeElement.value);
+	var muffinValue = Number(muffinElement.value)
 
+	if(muffinValue < cupcakeValue){
+		var giveMuffin = "Buy a muffin";
+		message.innerHTML = giveMuffin;
+	}
+	if(cupcakeValue < muffinValue ){
+		var giveCupcake = "Buy a cupcake";
+		message.innerHTML = giveCupcake;
+	}
 	// But a cupcake
-
 }
+checkbutton.addEventListener('click', whatToBuy)
